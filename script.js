75e07ac3218b0bb1a64e010321cb07b5b62373b5
Akash Malakar <AkashMalakar3.3@gmail.com>
2024-01-11 09:30:10 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,61 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+
+function page3Animation() {
+  var elemC = document.querySelector("#elem-container");
+  var fixed = document.querySelector("#fixed-image");
+  elemC.addEventListener("mouseenter", function () {
+    fixed.style.display = "block";
+  });
+  elemC.addEventListener("mouseleave", function () {
+    fixed.style.display = "none";
+  });
+  var elems = document.querySelectorAll(".elem");
+  elems.forEach(function (e) {
+    e.addEventListener("mouseenter", function () {
+      var image = e.getAttribute("data-image");
+      fixed.style.backgroundImage = `url(${image})`;
+    });
+  });
+}
+
+function swiperAnimation() {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 100,
+  });
+}
+function menuAnimation() {
+  var full = document.querySelector("#full-scr");
+  var menu = document.querySelector("nav h3");
+  var navimg = document.querySelector("nav img");
+  var flag = 0;
+  menu.addEventListener("click", function () {
+    if (flag == 0) {
+      full.style.top = 0;
+      navimg.style.opacity = 0;
+      menu.innerHTML = "X";
+      flag = 1;
+    } else {
+      full.style.top = "-100%";
+      navimg.style.opacity = 1;
+      menu.innerHTML = "Menu";
+      flag = 0;
+    }
+  });
+}
+
+function loaderAnimation() {
+  var loader = document.querySelector("#loader");
+  setTimeout(function () {
+    loader.style.top = "-100%";
+  }, 3500);
+}
+
+swiperAnimation();
+page3Animation();
+menuAnimation();
+loaderAnimation();
